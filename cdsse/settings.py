ad444dd5a8d798a0fd3fe6c69ce416b9dda24968
Emmanuel (Maxzeno) <mmnlchidera@gmail.com>
2022-11-18 12:12:02 +0100

    hopes high

--- cdsse/settings.py
+++ cdsse/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # This makes the app to use local db eg sqlite instead of production postgresql created by me
 
